4d74a7750647efc57f74d8534bc44e0b573c818d
Assortrock_10\assortrock10 <gunn0467@gmail.com>
2024-08-01 12:34:53 +0900

    [ProjectP] upperbody 동작 안되는 현상 수정

---
+++ Source/ProjectP/Inventory/Item/Equipment/Weapon/TwoHandSword.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WeaponItem.h"
+#include "TwoHandSword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTP_API ATwoHandSword : public AWeaponItem
+{
+	GENERATED_BODY()
+
+public:
+	ATwoHandSword();
+};
